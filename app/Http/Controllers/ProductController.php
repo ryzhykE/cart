5bd9b6e80c7c411946476cd71c6c43965167b140
stoyat <evgenii_r84@mail.ru>
2017-01-27 19:29:14 +0200

    ProductController fun getAddToCart

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Cart;
 use App\Product;
 use Illuminate\Http\Request;
+use Session;
 
 class ProductController extends Controller
 {
@@ -17,5 +19,12 @@ class ProductController extends Controller
     public function getAddToCart(Request $request, $id)
     {
         $product = Product::find($id);
+        $oldCart = Session::has('cart') ? Session::get('cart') : null;
+        $cart = new Cart($oldCart);
+        $cart->add($product, $product->id);
+
+        $request->session()->put('cart', $cart);
+        //dd($request->session()->get('cart'));
+        return redirect()->route('product.index');
     }
 }
