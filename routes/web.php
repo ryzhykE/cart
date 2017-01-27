8400e383da6d23912666c5bc904839951d28e873
stoyat <evgenii_r84@mail.ru>
2017-01-27 20:58:50 +0200

    Route checkout

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,11 @@ Route::get('/shopping-cart', [
     'as' => 'product.shoppingCart'
 ]);
 
+Route::get('/checkout', [
+    'uses' => 'ProductController@getCheckout',
+    'as' => 'checkout'
+]);
+
 Route::group(['prefix' => 'user'], function() {
     Route::group(['middleware' => 'guest'], function () {
         Route::get('/signup', [
